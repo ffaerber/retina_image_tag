58c7f6cc6727f5d63ae4ec98225481c331433496
Felix Faerber <mail@ffaerber.com>
2012-05-22 14:33:06 +0200

    fixing assetpipline bug

--- lib/retina_image_tag/view_helpers.rb
+++ lib/retina_image_tag/view_helpers.rb
@@ -1,7 +1,6 @@
 module RetinaImageTag
   module ViewHelpers
     
-    
     def retina_image( pixel_ratio )
       @retina_image_path = image_path(@file_name+"@#{pixel_ratio}x"+@file_ext)
       image_tag(@retina_image_path, @options)
@@ -11,10 +10,11 @@ module RetinaImageTag
     def retina_image_tag(image, options = {})
       @devicePixelRatio = cookies[:devicePixelRatio]
       @options          = options
+      @image            = image                             # foo.jpg
       
-      @file_path        = image_path(image)
-      @file_ext         = File.extname(@file_path)                # .jpg, .png
-      @file_name        = File.basename(@file_path, @file_ext)    # pic1, foo
+      @file_path        = image_path(@image)
+      @file_ext         = File.extname(@file_path)          # .jpg, .png
+      @file_name        = File.basename(@image, @file_ext)  # pic1, foo
       @file_dirname     = File.dirname(@file_path)
       
             
