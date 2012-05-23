49f3ff3faaca5fde169dd61f8e82126de24d2c7c
Felix Faerber <mail@ffaerber.com>
2012-05-23 22:29:28 +0200

    bug fix

--- lib/retina_image_tag/view_helpers.rb
+++ lib/retina_image_tag/view_helpers.rb
@@ -1,5 +1,3 @@
-require 'fastimage'
-
 module RetinaImageTag
   module ViewHelpers
     
@@ -9,32 +7,18 @@ module RetinaImageTag
     end
     
     
-    def full_image_path
-      if Rails.env == 'development'
-        Rails.root.to_s+'/app/assets/images/'+@image
-      else
-        request.protocol + request.host_with_port + image_path(@image)
-      end
-    end
-    
-    
     def retina_image_tag(image, options = {})
       @devicePixelRatio = cookies[:devicePixelRatio]
       @options          = options
       @image            = image             # foo.jpg, subdir/foo.png
 
-      
-      if @options[:size] == nil
-        @options[:size] = FastImage.size(full_image_path.delete '@2x').join("x")
-      end
-      
       case @devicePixelRatio
         when '2'
           retina_image 2
         when '1.5'
           retina_image 2
         else
-        image_tag(@image, @options)
+          image_tag(@image, @options)
        end
      end
      
