5caf8f436747371357953366d41c23167a7ebf79
Felix Faerber <mail@ffaerber.com>
2012-10-28 22:09:05 +0100

    changes for carrierwave

--- lib/retina_image_tag/view_helpers.rb
+++ lib/retina_image_tag/view_helpers.rb
@@ -1,24 +1,37 @@
 module RetinaImageTag
   module ViewHelpers
     
+
     def retina_image( pixel_ratio )
-      insert_on = -File.extname(@image).size-1
-      image_tag(@image.insert(insert_on, "@#{pixel_ratio}x"), @options)
+      if @options == {}
+        image_tag(@image.retina)
+      else
+        insert_on = -File.extname(@image).size-1
+        image_tag(@image.insert(insert_on, "@#{pixel_ratio}x"), @options)
+      end
     end
-    
-    
+
+
     def retina_image_tag(image, options = {})
-      @devicePixelRatio = cookies[:devicePixelRatio]
+      @devicePixelRatio = cookies[:devicePixelRatio].to_i
       @options          = options
-      @image            = image             # foo.jpg, subdir/foo.png
+      @image            = image
 
       case @devicePixelRatio
-        when '2'
+        when 4
           retina_image 2
-        when '1.5'
+        when 3
+          retina_image 2
+        when 2
+          retina_image 2
+        when 1.5
           retina_image 2
         else
-          image_tag(@image, @options)
+          if @options == {}
+            image_tag(@image.normal, @options)
+          else
+            image_tag(@image, @options)
+          end
        end
      end
      
