ad1e21d858927b726855f45ad3f67acbc6f7ad46
ffaerber <mail@ffaerber.com>
2012-12-20 17:38:13 +0100

    change CarrierWave version load

--- lib/retina_image_tag/view_helpers.rb
+++ lib/retina_image_tag/view_helpers.rb
@@ -1,16 +1,15 @@
 module RetinaImageTag
   module ViewHelpers
-    
+
 
     def retina_image( pixel_ratio )
-      if @image.class.superclass.superclass.to_s == "CarrierWave::Uploader::Base" 
+      if @image.class.superclass.superclass.to_s == "CarrierWave::Uploader::Base"
         image_tag(@image.retina, :size => @image.class.superclass.sizes[@image.version_name].join('x'))
       else
         insert_on = -File.extname(@image).size-1
         image_tag(@image.insert(insert_on, "@#{pixel_ratio}x"), @options)
       end
     end
-
 
     def retina_image_tag(image, options = {})
       @devicePixelRatio = cookies[:devicePixelRatio].to_i
@@ -27,14 +26,14 @@ module RetinaImageTag
         when 1.5
           retina_image 2
         else
-          if @image.class.superclass.superclass.to_s == "CarrierWave::Uploader::Base" 
+          if @image.class.superclass.superclass.to_s == "CarrierWave::Uploader::Base"
             image_tag(@image.normal)
           else
             image_tag(@image, @options)
           end
        end
      end
-     
+
   end
 end
 
