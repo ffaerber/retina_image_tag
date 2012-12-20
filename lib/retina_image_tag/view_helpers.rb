96f47f34147a584a0314562aab9a6b27d04d7a31
Felix Faerber <mail@ffaerber.com>
2012-12-20 19:51:49 +0100

    fix

--- lib/retina_image_tag/view_helpers.rb
+++ lib/retina_image_tag/view_helpers.rb
@@ -3,8 +3,8 @@ module RetinaImageTag
 
 
     def retina_image( pixel_ratio )
-      if @image.class.superclass.to_s == "CarrierWave::Uploader::Base"
-        image_tag(@image, :size => @image.class.superclass.sizes[@image.version_name].join('x'))
+      if @image.class.superclass.superclass.to_s == "CarrierWave::Uploader::Base"
+        image_tag(@image, :size => @image.class.sizes[@image.version_name].join('x'))
       else
         insert_on = -File.extname(@image).size-1
         image_tag(@image.insert(insert_on, "@#{pixel_ratio}x"), @options)
@@ -26,7 +26,7 @@ module RetinaImageTag
         when 1.5
           retina_image 2
         else
-          if @image.class.superclass.to_s == "CarrierWave::Uploader::Base"
+          if @image.class.superclass.superclass.to_s == "CarrierWave::Uploader::Base"
             image_tag(@image.normal)
           else
             image_tag(@image, @options)
